aec7c4e00a86fa3609f66fa8d2fa943e3f48f301
RobertoMendezAlfaro <roberto.mendezalfaro@ucr.ac.cr>
2024-10-25 17:34:24 -0600

    updated python script

---
+++ codigos/ecuacion_calor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import numpy as np
+
+T(x,y,t)
+def funcionCalor(x,y,t):
+#L longitud de x
+
+
+
+#B longitud de y
+
+
+
+#c = 1  para simplificar
+c = 1
+
+T(x,y,t+1) = T(x,y,t) + ((c**2)*(delta_t / delta_x**2) * (T(x+1,y,t) - 2*T(x,y,t) + T(x-1,y,t)) + ((c**2) * delta_t /
