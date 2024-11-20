b1d8741595200aa34d1436455264c71f2fbe313d
Roberto Méndez <roberto.mendezalfaro@ucr.ac.cr>
2024-11-20 17:28:03 -0600

    Se modificó la función para que devolviera un arreglo con copias del sistema conforme se va iterando en el

--- codigos/ecuacion_calor.py
+++ codigos/ecuacion_calor.py
@@ -1,63 +1,117 @@
 #!/usr/bin/env python
 
 import numpy as np # Biblioteca para calculos matematicos
+import matplotlib
+from matplotlib import cm
 import matplotlib.pyplot as plt
-import matplotlib.cm as cm # Biblioteca para crear gáficos
+from matplotlib.animation import FuncAnimation, FFMpegWriter
+#import matplotlib.cm as cm # Biblioteca para crear gáficos
 
-def temperaturas(cond_inicial, ancho, alto, omega):
+def temperaturas(temp_sup, temp_lat, temp_init, ancho, alto, omega, frames):
 
-    phi = np.zeros((ancho + 1, alto + 1), dtype=float) # Grilla bidimensional 100 x 200
+    phi = np.zeros((ancho + 1, alto + 1), dtype=float) # Grilla bidimensional
 
-    phi[0,:] = cond_inicial # Condición inical en borde derecho de grilla
+    phi[:, :] = temp_init # Condición inicial en el interior del sistema
+
+    phi[0, :] = temp_sup # Condición inical en borde superior de grilla
+
+    phi[:, 0] = temp_lat # Condición inical en borde izquierdo de grilla
+
+    phi[0, 0] = (temp_sup + temp_lat) / 2 # Condición inicial en la esquina
 
     phi_copy = phi.copy() # Para comparar el error con la nueva grilla
 
-    delta = 1.0 # Diferencia entre phi inicial y el modificado con los valores de T
+    animation = np.zeros((frames, ancho, alto))
 
-    while delta > 1e-5: # Verifica que el error sea sumamente pequeño
+    iterations = -1 # Contador para las iteraciones
 
-        for i in range(ancho + 1):
+    iterations_per_frame = 200 # Cada cuantas iteraciones se desea guarda una copia del sistema
 
-            for j in range(alto + 1):
+    frame_counter = 0
 
-                if i == 0 or i == ancho or j == 0 or j == alto: # Condición de frontera: T = 280 K en bordes
-                    phi[i, j] = 280
+    delta = 1
 
-                else: # Método Gauss Seidel para aproximar el cambio de T en el espacio con el tiempo
+    while delta > 1e-5: #
+        iterations += 1
+        # Método Gauss Seidel para aproximar el cambio de T en el espacio con el tiempo
+        for i in range(1, ancho):
 
-                    phi[i,j] = (1 + omega) * 0.25 * (phi[i + 1, j] + phi[i - 1, j] + phi[i, j + 1] + phi[i, j - 1]) - omega * phi[i,j]
+            for j in range(1, alto):
 
-        delta = np.max(np.abs(phi - phi_copy)) # Estimamos la diferencia
+                phi[i,j] = (1 + omega) * 0.25 * (phi[i + 1, j] + phi[i - 1, j] + phi[i, j + 1] + phi[i, j - 1]) - omega * phi[i, j]
 
-        phi_copy = phi.copy() # Ponemos la nueva grilla como la antigua para compararlo con la nueva en la siguiente iteración
+        # Se guarda una copia del estado del sistema para crear la animación
+        if frame_counter < frames:
+            if (iterations % iterations_per_frame) == 0:
 
-    return phi
+                animation[frame_counter, :, :] = phi[:-1, :-1]
 
-# Condión Inicial 1: Ti = 0
-fourier_t = np.fft.fft(temperaturas(0, 10, 20 ,0), norm = "forward" ) # Optimizamos el cálculo usando transformadas de fourier para evaluar la temperatura en cada punto de la grilla
+                frame_counter += 1
 
-plt.imshow(fourier_t)
-plt.gray()
+
+        delta = np.max(np.abs(phi - phi_copy))
+
+        phi_copy = phi.copy() # Ponemos la nueva grilla como la antigua para compararlo con la  nueva en la siguiente iteración
+    print(iterations)
+    return animation
+
+
+#heatmap = temperaturas(100, 200, 20, 40, 80, 0, 10)
+
+#fig = plt.figure()
+#line, = plt.plot([])
+
+#def animate(frame):
+ #   images = heatmap[frame, :, :]
+  #  line.set_data(images)
+
+#anim = FuncAnimation(fig, animate, frames = 10)
+#video = anim.to_html5_video()
+#html = display.HTML(video)
+#display.display(html)
+#plt.close()
+
+
+
+
+#print(temperaturas(100, 200, 20, 40, 80, 0, 10))
+
+
+plt.imshow(temperaturas(100, 200, 20, 40, 80 ,0, 10)[9, :, :], cmap="inferno")
 plt.show()
 
-# Condión Inicial 2: Ti = 50 K
-fourier_t = np.fft.fft(temperaturas(50, 10, 20 ,0), norm = "forward" )
 
-plt.imshow(fourier_t)
-plt.gray()
-plt.show()
+#writer = FFMpegWriter(fps=15)
 
-# Condión Inicial 3: Ti = 170 K
-fourier_t = np.fft.fft(temperaturas(170, 10, 20 ,0), norm = "forward" )
+#fig, ax = plt.subplots()
 
-plt.imshow(fourier_t)
-plt.gray()
-plt.show()
+#with writer.saving(fig, "heatmap.mp4", 100):
+#    for frame in range(30): # Range = numbers of frames used for animation
 
-# Condión Inicial 4: Ti = 300 K
-fourier_t = np.fft.fft(temperaturas(300, 10, 20 ,0), norm = "forward" )
 
-plt.imshow(fourier_t)
-plt.gray()
-plt.show()
+
+#        heatmap = temperaturas(100, 10, 20, 0, 10 ,20, 30)
+
+ #       ax.plot(heatmap[frame])
+
+  #      writer.grab_frame()
+#        plt.cla()
+
+
+
+
+#temperaturas(100, 40, 80, 0, 10, 2600)
+
+#fig = plt.figure()
+#line, = plt.plot([])
+#plt.xlabel(r'$x$')
+#plt.ylabel(r'$\phi(x,t)$')
+#plt.xlim(0.0, 1.0)
+#plt.ylim(-0.05, 0.05)
+
+#anim = FuncAnimation(fig, temperaturas(100, 40, 80, 0, 10, 2600, 30), frames = 30, interval = 10)
+#video = anim.to_html5_video()
+#html = display.HTML(video)
+#display.display(html)
+#plt.close()
 
