2ce2ba677197dc2aa99773e1463877adaca29d7e
Daniela438 <daniela.fonsecagranados@ucr.ac.cr>
2024-11-25 09:51:08 -0600

    Definir los parámetros de entrada en main.cpp

--- codigos/main.cpp
+++ codigos/main.cpp
@@ -1,2 +1,23 @@
 #include <iostream>
 #include "ecuacion_calor.hpp"
+
+int main(){
+
+  //Parámetros de entrada
+  double temp_sup = 100.0;
+  double temp_lat = 200.0;
+  double temp_init = 20.0;
+  int ancho = 40;
+  int alto = 80;
+  double omega = 0.0;
+  int iteraciones = 0;
+
+  //LLamar a la funcion para calcular temperaturas
+  
+
+  std::cout << "Número de iteraciones total: " << iteraciones << std::endl;
+
+
+
+  return 0;
+}
